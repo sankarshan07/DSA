17a6ab016e1f2d5b6e745ae1da83cf6f06a3c4aa
sankarshan <sankarshandesai3107@gmail.com>
2024-05-15 21:50:36 +0530

    sumof2

---
+++ sumoftwo.java
@@ -0,0 +1,16 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class sumoftwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+ b;
+        System.out.println("the sum of a and b is "+c);
+
+
+
+    }
+}
