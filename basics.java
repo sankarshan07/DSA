40e772efb9686f20310737e16e2b048afd7a3fc8
sankarshan <sankarshandesai3107@gmail.com>
2024-05-15 20:53:20 +0530

    basics

---
+++ basics.java
@@ -0,0 +1,7 @@
+package DSA;
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("hello sankarshan");
+        System.out.println("hello welcome to java DSA");
+    }
+}
