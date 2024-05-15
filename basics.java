e9e23d7195f6e5de087fa71a96877d756ff85895
sankarshan <sankarshandesai3107@gmail.com>
2024-05-15 21:44:49 +0530

    bsics

--- basics.java
+++ basics.java
@@ -1,7 +1,21 @@
 package DSA;
 public class basics {
     public static void main(String[] args) {
-        System.out.println("hello sankarshan");
+            System.out.println("hello ");
         System.out.println("hello welcome to java DSA");
+        int  a = 10;
+        int b = 20;
+        int c= a+ b;
+        String d ="name";
+        byte e = 8;
+        boolean m = true;
+        char h ='b';
+        float f = 15.5f;
+        System.out.println(h);
+
+
+
+        System.out.println(c);
+
     }
-}
+} 
---
+++ scannerclass.java
@@ -0,0 +1,15 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class scannerclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int b =  sc.nextInt();
+        String s = sc.nextLine();
+//        System.out.println(b);
+        System.out.println("your string is "+s);
+
+    }
+    
+}
