dc9803208c6dcedd4c77f8d1c5e5cc7b70b31e4e
sankarshan <sankarshandesai3107@gmail.com>
2024-05-15 22:05:43 +0530

    type conversions

---
+++ type_conversions.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class type_conversions {
+    public static void main(String[] args) {
+        int m = (int)(99.99f);
+        System.out.println(m);
+        float f = 25.0f;
+        int b = (int)f;
+        System.out.println(b);
+
+    }
+}
