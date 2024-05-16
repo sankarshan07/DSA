dc0cec855bbab75a6334c296686d87b9aebe5f90
sankarshan <sankarshandesai3107@gmail.com>
2024-05-16 21:29:48 +0530

    switch case problem

---
+++ consitionalStatement/switchCases.java
@@ -0,0 +1,19 @@
+package DSA.consitionalStatement;
+
+public class switchCases {
+    public static void main(String[] args) {
+        int num =2;
+        switch (num) {
+            case 1:
+                System.out.println("samosa");
+                break;
+            case 2:
+                System.out.println("dosa");
+                break;
+            default:
+                System.out.println("waterbottle");
+        }
+
+
+    }
+}
