3fd23c9a5903416bb7fe3c28ba2d28d2bfab15ef
sankarshan <sankarshandesai3107@gmail.com>
2024-05-16 21:03:01 +0530

    conditonal statement-problrms-3

---
+++ consitionalStatement/largestOfThree.java
@@ -0,0 +1,24 @@
+package DSA.consitionalStatement;
+
+import java.util.Scanner;
+//largest of threee numbers
+public class largestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a>b && a>c){
+            System.out.println("a is greater");
+
+        } else if (b> c) {
+            System.out.println("b is greater");
+
+        }
+        else{
+            System.out.println("c is greater");
+        }
+
+
+    }
+}
