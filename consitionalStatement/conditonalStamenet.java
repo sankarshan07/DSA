825e0c237f2d5cef82e2ae0a37dabd3105147aaa
sankarshan <sankarshandesai3107@gmail.com>
2024-05-16 20:21:21 +0530

    conditonal statement

---
+++ consitionalStatement/conditonalStamenet.java
@@ -0,0 +1,16 @@
+package DSA.consitionalStatement;
+
+public class conditonalStamenet {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        if (a>b){
+            System.out.println("a is greater");
+
+        }
+        else {
+            System.out.println("b is greater");
+
+        }
+    }
+}
