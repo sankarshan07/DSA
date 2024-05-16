8bcbf220781c5568c7c2c622bd2e5a527fa0e257
sankarshan <sankarshandesai3107@gmail.com>
2024-05-16 21:10:57 +0530

    ternary-operator

---
+++ consitionalStatement/ternaryOperator.java
@@ -0,0 +1,11 @@
+package DSA.consitionalStatement;
+
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b  =15;
+
+        String c = (a>b)?"a is greater":"b is greater";
+        System.out.println(c);
+    }
+}
