2a3c8d305e0e4a54240efe185f827a4a7ad85830
sankarshan <sankarshandesai3107@gmail.com>
2024-05-16 22:13:50 +0530

    calci

--- switchcalculator.java
+++ switchcalculator.java
@@ -1,2 +1,28 @@
-package DSA;public class switchcalculator {
+package DSA;
+
+import java.util.Scanner;
+
+public class switchcalculator {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int a = sc.nextInt();;
+        int b = sc.nextInt();;
+
+        System.out.println("enter your choice");
+        int ch = sc.nextInt();
+        switch (ch){
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a*b);
+                break;
+            case 3:
+                System.out.println(b/a);
+                break;
+            default:
+                System.out.println("");
+        }
+
+    }
 }
