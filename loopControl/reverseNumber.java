238eef49346e0f8f2e86cd671793bebc9511d4ed
sankarshan <sankarshandesai3107@gmail.com>
2024-05-17 23:29:47 +0530

    do-while

--- loopControl/reverseNumber.java
+++ loopControl/reverseNumber.java
@@ -1,7 +1,21 @@
 package DSA.loopControl;
 
+import java.util.Scanner;
+
 public class reverseNumber {
     public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("enter the number to be reverse");
+        int n  = sc.nextInt();  //rev =0
+        while (n>0){
+        int lastdigit = n %10;
+            System.out.print(lastdigit);   //rev =(rev*10)+lastdigit  for palindrome
+            n= n/10;
+
+        }
+
+
+
 
     }
 }
