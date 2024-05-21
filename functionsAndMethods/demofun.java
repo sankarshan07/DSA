f2504228a6531ebba862c02789f6eb2eaac05051
sankarshan <sankarshandesai3107@gmail.com>
2024-05-21 20:37:58 +0530

    static funcitons

--- functionsAndMethods/demofun.java
+++ functionsAndMethods/demofun.java
@@ -1,7 +1,24 @@
 package DSA.functionsAndMethods;
 
+import java.util.Scanner;
+
 public class demofun {
+    public static  void printHW(){  //static function
+        System.out.println("hwllo world");
+    }
+  static void sumoftwo(int a,int b){
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
+//        printHW();   //function call
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sumoftwo(a,b);
+
+
 
     }
 }
+
